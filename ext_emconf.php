2a0ae89431a255a199b2cc23de32a7e571713212
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-27 14:26:48 +0000

    Released version 0.3.1 to TER with warning about incompatibility with Tesseract
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/datadisplay/trunk@38683 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "datadisplay"
+# Extension Manager/Repository config file for ext "datadisplay".
 #
-# Auto generated 08-05-2008 11:15
+# Auto generated 27-09-2010 16:26
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Francois Suter (Cobweb)',
 	'author_email' => 'typo3@cobweb.ch',
 	'shy' => '',
-	'dependencies' => '',
+	'dependencies' => 'displaycontroller',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.3.0',
+	'version' => '0.3.1',
 	'constraints' => array(
 		'depends' => array(
 			'displaycontroller' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"b93a";s:10:"README.txt";s:4:"3a34";s:12:"ext_icon.gif";s:4:"1b66";s:17:"ext_localconf.php";s:4:"5cfe";s:14:"ext_tables.php";s:4:"6523";s:14:"ext_tables.sql";s:4:"4027";s:15:"flexform_ds.xml";s:4:"bac6";s:16:"locallang_db.xml";s:4:"8a0b";s:17:"locallang_tca.xml";s:4:"0ffb";s:32:"pi1/class.tx_datadisplay_pi1.php";s:4:"eb92";s:20:"pi1/static/setup.txt";s:4:"ef04";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"b93a";s:10:"README.txt";s:4:"5241";s:8:"TODO.txt";s:4:"967d";s:24:"class.tx_datadisplay.php";s:4:"7c3e";s:12:"ext_icon.gif";s:4:"1b66";s:17:"ext_localconf.php";s:4:"beba";s:14:"ext_tables.php";s:4:"ff9f";s:14:"ext_tables.sql";s:4:"31cf";s:15:"flexform_ds.xml";s:4:"bac6";s:16:"locallang_db.xml";s:4:"a322";s:17:"locallang_tca.xml";s:4:"0ffb";s:7:"tca.php";s:4:"5dac";s:36:"res/icons/add_datadisplay_wizard.gif";s:4:"66fc";s:42:"res/icons/icon_tx_datadisplay_displays.gif";s:4:"1b66";s:16:"static/setup.txt";s:4:"c83b";}',
 	'suggests' => array(
 	),
 );
